687017bfcdc30e70d32468be61fa2967ee6b5596
Luca Ferroni <luca@befair.it>
2016-07-11 00:28:50 +0200

    [ICONS] fix icon-yes and icon-no in bool_img templatetag

--- base/templatetags/basic_tags.py
+++ base/templatetags/basic_tags.py
@@ -7,7 +7,7 @@ from django.conf import settings
 @register.simple_tag
 def bool_img(value):
     if bool(value):
-        rv = '<img alt="True" src="%simg/admin/icon-yes.gif">' % settings.ADMIN_MEDIA_PREFIX
+        rv = '<img alt="True" src="%sstatic/admin/img/icon-yes.svg">' % settings.STATIC_URL
     else:
-        rv = '<img alt="False" src="%simg/admin/icon-no.gif">' % settings.ADMIN_MEDIA_PREFIX
+        rv = '<img alt="False" src="%sstatic/admin/img/icon-no.svg">' % settings.STATIC_URL
     return rv
